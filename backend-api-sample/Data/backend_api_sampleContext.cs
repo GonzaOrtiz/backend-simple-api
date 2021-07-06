650ebb2add51d6ebd076500510e9778ac357c302
GonzaOrtiz <gonzaortiz1506@gmail.com>
2021-07-06 16:53:37 -0300

    creando repositorio backend Api

---
+++ backend-api-sample/Data/backend_api_sampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using backend_api_sample.Models;
+
+namespace backend_api_sample.Data
+{
+    public class backend_api_sampleContext : DbContext
+    {
+        public backend_api_sampleContext (DbContextOptions<backend_api_sampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<backend_api_sample.Models.Persona> Persona { get; set; }
+    }
+}
